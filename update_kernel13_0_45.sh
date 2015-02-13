62fe8e0074ea8dd9669c335ee23ad9f199ce501e
spandanb <spandan@savinetwork.ca>
2015-02-13 09:47:21 -0500

    Updated kernel 13

--- update_kernel13_0_45.sh
+++ update_kernel13_0_45.sh
@@ -4,7 +4,9 @@
 
 cd /tmp/
 
-wget http://launchpadlibrarian.net/194803620/linux-headers-3.13.0-45_3.13.0-45.74_all.deb
+wget http://security.ubuntu.com/ubuntu/pool/main/l/linux/linux-headers-3.13.0-45-generic_3.13.0-45.74_amd64.deb
+wget http://security.ubuntu.com/ubuntu/pool/main/l/linux/linux-headers-3.13.0-45_3.13.0-45.74_all.deb
+wget http://security.ubuntu.com/ubuntu/pool/main/l/linux/linux-image-3.13.0-45-generic_3.13.0-45.74_amd64.deb
 
 sudo dpkg -i linux-headers-3.13.0-*.deb 
 
